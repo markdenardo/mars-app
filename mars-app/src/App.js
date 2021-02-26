9ff31d100bf03acdd58f29dc1903d16c686addad
markdenardo <mark.denardo@gmail.com>
2021-02-26 13:21:13 -0500

    store photos to redux

--- mars-app/src/App.js
+++ mars-app/src/App.js
@@ -6,24 +6,27 @@ import { getPhotos } from "./redux/actions";
 import './App.css';
 
 function App() {
-  const photos = useSelector((state) => state.photos);
+  const photos = useSelector(getPhotos());
+  console.log("photos", photos);
   const dispatch = useDispatch();
    useEffect(() => {
-     dispatch(getPhotos());
-   }, []);
+     dispatch(getPhotos(photos));
+   }, [dispatch, photos]);
 
-  const photosList = () => {
-    // return photos.map(
-    //   (photo) => 
-    //   { <img alt={photo[0].img_src}/> } 
-    //   )
-    return <div>poop</div>  
+  const PhotosList = () => {
+    
+    // if (photos !== undefined){
+    // return photos.map((photo) => {
+    // <img alt={photo[0].img_src} />;
+    // });
+    // }
+    // return <div>poop</div>  
   }
 
 
   return (
     <div className="App">
-     <photosList/>
+     {/* <PhotosList/> */}
     </div>
   );
 }
--- mars-app/src/redux/actions.js
+++ mars-app/src/redux/actions.js
@@ -1,16 +1,35 @@
-import axios from "axios";
+// import axios from "axios";
+
+// export const getPhotos = () => {
+//   try {
+//     return (dispatch) => {
+//       axios
+//         .get(
+//           "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=x093fwocm05tLMOCe53njaCSzzbwMk9VG85yW18J"
+//         )
+//         .then((response) => {
+//           console.log("response.data:", response.data);
+//         })
+//         .then(photos => dispatch({ type: "GET_PHOTOS", photos: photos }));
+//     };
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
 
 export const getPhotos = () => {
   try {
-    return (dispatch) => {
-      axios
-        .get(
+    return dispatch => {
+      // dispatch(loadingTrue())
+      return (
+        fetch(
           "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=x093fwocm05tLMOCe53njaCSzzbwMk9VG85yW18J"
         )
-        .then((response) => {
-          console.log("response.data:", response.data);
-        })
-        .then((photos) => dispatch({ type: "GET_PHOTOS", photos: photos }));
+          .then((r) => r.json())
+          // .then(movies => dispatch(setMovies(movies), dispatch(loadingFalse())))
+          .then((photos) => dispatch({ type: "GET_PHOTOS", photos: photos }))
+      );
     };
   } catch (error) {
     console.log(error);
