406c52e1d71602bbe2f4e6c422ac71b8c5136cee
markdenardo <mark.denardo@gmail.com>
2021-02-26 19:08:51 -0500

    nasa api working

--- mars-app/src/App.js
+++ mars-app/src/App.js
@@ -1,32 +1,29 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getPhotos } from "./redux/actions";
-
-
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const photos = useSelector(getPhotos());
-  console.log("photos", photos);
-  const dispatch = useDispatch();
-   useEffect(() => {
-     dispatch(getPhotos(photos));
-   }, [dispatch, photos]);
+  const [photo, setPhoto] = useState();
 
-  const PhotosList = () => {
-    
-    // if (photos !== undefined){
-    // return photos.map((photo) => {
-    // <img alt={photo[0].img_src} />;
-    // });
-    // }
-    // return <div>poop</div>  
-  }
+  const hook = () => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=x093fwocm05tLMOCe53njaCSzzbwMk9VG85yW18J&count=1")
+      .then((response) => {
+        setPhoto(response.data); 
+        // debugger
+      });
+  };
 
-
-  return (
+  useEffect(hook, []);
+  return(
     <div className="App">
-     {/* <PhotosList/> */}
+      {(photo == null)
+          ? <div> Loading </div> 
+          : <div> <h1>{photo[0].title}</h1>
+        <img src={photo[0].url} />
+        <p>{photo[0].explanation}</p> 
+      <button onClick={hook}>pic</button>
+      </div>}
     </div>
   );
 }
--- mars-app/src/redux/reducer.js
+++ mars-app/src/redux/reducer.js
@@ -2,6 +2,7 @@ const photoReducer = (
   state = { photos: [], photo: {}, loading: false },
   action
 ) => {
+    
   switch (action.type) {
     case "GET_PHOTOS":
       return {
